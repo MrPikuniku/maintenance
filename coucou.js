ab6b8b21c1c680e8aa40107ac039bd916dcfb7c7
unknown <sangaremoon@gmail.com>
2025-02-12 22:44:34 +0100

    Exercice V: Question 10

--- coucou.js
+++ coucou.js
@@ -1,6 +1,10 @@
 
 const jeSersARien = 42;
 
+const age = 20;
+const message = age >= 18 ? 'Majeur' : 'Mineur';
+console.log(message);
+
 function coucou() {
   console.log("Coucou, c'est moi, le changement !");
 }
@@ -12,7 +16,12 @@ function coucouPerso(name) {
 coucouPerso('SANGARE Mamourou');
 
 function coucouQuote() {
+  // eslint-disable-next-line no-console
   console.log('Coucou, je préfère les quotes simples !');
 }
 coucouQuote();
+
+// TODO: Implémenter une meilleure logique ici
+console.log('Test de la règle no-warning-comments');
+
 alert("Alerte peu autorisée sur le serveur "); 
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+const isBuild = process.env.NODE_ENV === "production";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,7 +11,10 @@ export default [
   {rules:{
     "no-alert": "error",
     "no-ternary" : "warn",
-    "no-console" : "warn"
+    "no-console" : "warn",
+    "no-console": isBuild ? "error" : "warn", // console.log() autorisé en dev, interdit en build
+    "eqeqeq": isBuild ? "error" : "off",      // === obligatoire en build
+    "no-warning-comments": isBuild ? "error" : "off"
   } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
